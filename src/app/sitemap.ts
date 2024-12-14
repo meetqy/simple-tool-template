4d488801a876e66362ab2f1280c5d63d6917659c
meetqy <meetqy@icloud.com>
2024-12-14 11:31:27 +0800

    Refactor sitemap structure to use categories for better organization of URLs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,18 @@ import type { MetadataRoute } from "next";
 import { generateAlternates } from "~/utils/generate-alternates";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    "/financial/auto-loan-payoff-calculator",
-    "/media/audiobook-speed-calculator",
-    "/construction/asphalt-calculator",
-    "/health/baby-eye-color-calculator",
-    "/game/kd-calculator-kill-to-death-ratio",
-    "/transportation/veaero-calculator",
-  ].map((href) => {
+  const categories = {
+    financial: ["/financial/auto-loan-payoff-calculator"],
+    media: ["/media/audiobook-speed-calculator"],
+    construction: ["/construction/asphalt-calculator"],
+    health: ["/health/baby-eye-color-calculator"],
+    game: ["/game/kd-calculator-kill-to-death-ratio"],
+    transportation: ["/transportation/veaero-calculator"],
+  };
+
+  const urls = Object.values(categories).flat();
+
+  return urls.map((href) => {
     const { alternates } = generateAlternates(href);
     return {
       url: `https://howtocalculator.com${href}`,
